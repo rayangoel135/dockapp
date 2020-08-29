f4961e618b864436d5faf55c1c307d5396608440
ravigoel <ravigoel135@gmail.com>
2020-08-29 07:19:25 -0400

    test

--- src/main/java/com/docker/lambda/LambdaAPIHandler.java
+++ src/main/java/com/docker/lambda/LambdaAPIHandler.java
@@ -5,10 +5,10 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.docker.entity.Request;
 import com.docker.entity.Response;
 
-public class LambdaAPIHandler implements RequestHandler<Request, com.docker.entity.Response> {
+public class LambdaAPIHandler implements RequestHandler<Void, com.docker.entity.Response> {
 
 	@Override
-	public Response handleRequest(Request input, Context context) {
+	public Response handleRequest(Void input, Context context) {
 		// TODO Auto-generated method stub
 		return new Response("Success");
 	}
