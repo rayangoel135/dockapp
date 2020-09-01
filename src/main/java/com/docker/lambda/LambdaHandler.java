7fef7deeff131429a69dbe8639d29bb0dc9b0b7d
ravigoel <ravigoel135@gmail.com>
2020-09-01 12:39:10 -0400

    test

--- src/main/java/com/docker/lambda/LambdaHandler.java
+++ src/main/java/com/docker/lambda/LambdaHandler.java
@@ -2,11 +2,13 @@ package com.docker.lambda;
 
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Service;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.docker.DockerServiceApplication;
+import com.docker.config.DockerConfig;
 import com.docker.controller.MicroServiceController;
 import com.docker.entity.Request;
 import com.docker.entity.Response;
@@ -20,7 +22,9 @@ public class LambdaHandler implements RequestHandler<Void, Response>{
 	@Override
 	public Response handleRequest(Void input, Context context) {
 		// TODO Auto-generated method stub
-		DockerServiceApplication.main(new String[] {});
+		//DockerServiceApplication.main(new String[] {});
+		AnnotationConfigApplicationContext cont = new AnnotationConfigApplicationContext(DockerConfig.class);		
+		MicroServiceController controller = cont.getBean(MicroServiceController.class);
 		LOGGER.info("Inside lambda");
 		controller.testDockerService();
 		return new Response("success");
